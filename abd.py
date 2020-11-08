ffe29183999373d5922965d3f230ea16c3c9d098
KienKey <33190277+KienKey@users.noreply.github.com>
2020-11-08 23:26:48 +0700

    update somes text

---
+++ abd.py
@@ -0,0 +1,9 @@
+def main():
+    print("Hello World")
+
+    x = "How Are You"
+    y = x.upper()
+    print(y)
+
+if __name__ == "__main__":
+    main()
---
+++ test.py
@@ -0,0 +1,8 @@
+import pygame
+
+pygame.init()
+# Create the screen
+screen = pygame.display.set_mode((800,500))
+
+while True:
+	pass
